ea8972629b6b6c58946018a4dcf51974d1d24d53
Somraj Mukherjee <iamsomraj@gmail.com>
2021-05-31 20:40:52 +0530

    About Section comp created

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,11 @@
+import AboutUs from "./pages/AboutUs";
+
 function App() {
-  return <div className="App">Film</div>;
+  return (
+    <div className="App">
+      <AboutUs />
+    </div>
+  );
 }
 
 export default App;
---
+++ src/pages/AboutUs.tsx
@@ -0,0 +1,11 @@
+import AboutSection from "../components/AboutSection";
+
+const AboutUs: React.FC = () => {
+  return (
+    <div>
+      <AboutSection />
+    </div>
+  );
+};
+
+export default AboutUs;
