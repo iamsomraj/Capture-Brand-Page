dc78056107f79ab94ca56cabee47969cba451bba
Somraj Mukherjee <iamsomraj@gmail.com>
2021-05-31 20:51:16 +0530

    services and faq section added

--- src/pages/AboutUs.tsx
+++ src/pages/AboutUs.tsx
@@ -1,10 +1,14 @@
 import AboutSection from "../components/AboutSection";
+import FaqSection from "../components/FaqSection";
+import ServicesSection from "../components/ServicesSection";
 
 const AboutUs: React.FC = () => {
   return (
-    <div>
+    <>
       <AboutSection />
-    </div>
+      <ServicesSection />
+      <FaqSection />
+    </>
   );
 };
 
